17e9a256b599d598a1fdbecf6f60d9722a80c953
Sujoy Sarkar <sujoysarkar96@gmail.com>
2023-02-04 17:39:59 -0500

    WhileLoop Demo

---
+++ src/WhileLoopDemo/WhileLoopDemo.java
@@ -0,0 +1,15 @@
+package WhileLoopDemo;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		
+		int i = 0; 
+
+		while (i < 5)
+			{
+				System.out.println("My name is Joy");
+				i++;
+			}
+	}	
+}
